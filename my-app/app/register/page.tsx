32ee329a81aacdfd83c8dc928a2f7de9e06b98c7
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-09-05 14:24:14 +0200

    modify the design of the register

--- my-app/app/register/page.tsx
+++ my-app/app/register/page.tsx
@@ -1,18 +1,16 @@
-
 import { ModeToggle } from "@/components/toggle.mode";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { redirect } from "next/navigation";
-import React from 'react';
 import { register } from "../action/user";
+import { Button } from "@/components/ui/button";
 
 const Register = async () => {
-
   return (
     <div className="relative mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212] dark:bg-black">
       {/* ModeToggle positioned at the top right */}
       <div className="absolute top-4 right-4">
+        <ModeToggle />
       </div>
 
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
@@ -48,30 +46,33 @@ const Register = async () => {
           </div>
         </div>
 
-        <Label htmlFor="email">Email</Label>
-        <Input
-          id="email"
-          placeholder="exemple@gmail.com"
-          type="email"
-          name="email"
-        />
+        <div className="flex flex-col mb-5">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            placeholder="exemple@gmail.com"
+            type="email"
+            name="email"
+          />
+        </div>
 
-        <Label htmlFor="password">Password</Label>
-        <Input
-          id="password"
-          placeholder="***********"
-          type="password"
-          name="password"
-          className="mb-5"
-        />
+        <div className="flex flex-col mb-5">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            placeholder="***********"
+            type="password"
+            name="password"
+          />
+        </div>
 
-        <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">
-          Sign up &rarr;
-        </button>
-
+        {/* P tag for login link below the inputs */}
         <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Already have an account? <Link href="/login">Login</Link>
+          Déjà un compte? <Link href="/login" className="text-blue-500">Login</Link>
         </p>
+
+        {/* Align the button to the right */}
+        <Button type="submit" className="ml-auto block">Sign up &rarr;</Button>
       </form>
     </div>
   );
