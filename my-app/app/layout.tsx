9c9ac137e3f9dd6e63965e305138a1f748bd6f77
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-09-10 16:33:56 +0200

    Just modify the redirect to home

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SessionProvider } from "next-auth/react"; // Importer SessionProvider
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FlipIt",
-  description: "Swith your style",
+  description: "Switch your style",
 };
 
 export default function RootLayout({
@@ -18,14 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import Settings from "@/components/settings/user_settings"
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <div>
-      <Settings />
+      <Header />
     </div>
   )
 }
