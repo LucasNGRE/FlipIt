289f00252610b635faeda7031bb63841bfd55831
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-09-26 11:16:31 +0200

    add the responsive on the chat components and reorganize the components directory in vsc

--- my-app/app/inbox/page.tsx
+++ my-app/app/inbox/page.tsx
@@ -1,21 +1,24 @@
-import ChatComponent from '@/components/chat-component'
-import ConversationList from '@/components/conversation-list'
-import React from 'react'
+import ChatComponent from '@/components/chat/chat-component';
+import ConversationList from '@/components/chat/conversation-list';
+import React from 'react';
 
 const Inbox = () => {
   return (
-    <div className="flex h-screen">
-      {/* Conversation list (left side) */}
-      <div className="w-1/4 bg-background border-r border-gray-300 overflow-y-auto">
+    <div className="flex flex-col h-screen lg:flex-row">
+      {/* Liste de conversations (côté gauche) */}
+      <div className="w-full lg:w-1/4 bg-background border-b lg:border-r border-gray-300 overflow-y-auto lg:h-auto">
         <ConversationList />
       </div>
 
-      {/* Chat component (right side) */}
-      <div className="w-3/4 flex flex-col bg-background">
-        <ChatComponent />
+      {/* Composant de chat (côté droit) */}
+      <div className="lg:w-3/4 flex flex-col bg-background lg:h-auto">
+        {/* Afficher le composant de chat uniquement sur les écrans plus grands */}
+        <div className="hidden lg:block">
+          <ChatComponent />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inbox
+export default Inbox;
