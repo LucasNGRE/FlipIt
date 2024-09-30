f8209a1990f257c6a0b5ea51d76eed78af1322b1
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-09-30 12:37:43 +0200

    commit

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -7,11 +7,6 @@ import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "FlipIt",
-//   description: "Switch your style",
-// };
-
 export default function RootLayout({
   children,
 }: Readonly<{
