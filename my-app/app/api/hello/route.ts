4d692c300bcfdd2d14a3139a6d015402477d265e
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-10-03 10:49:32 +0200

    ras

--- my-app/app/api/hello/route.ts
+++ my-app/app/api/hello/route.ts
@@ -1,5 +1,19 @@
-export async function GET(request: Request) {
-    return new Response(JSON.stringify({ message: 'Hello, world!' }), {
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from "@/auth";
+import { currentUser } from '@/lib/auth';
+
+export async function GET(request: NextRequest) {
+  const session = await currentUser();
+    // Vérifie si la session est valide
+    if (!session) {
+      return new NextResponse(JSON.stringify({ message: 'Unauthorized' }), {
+        status: 401,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  
+    // Si l'utilisateur est authentifié, continue avec la logique de la réponse
+    return new NextResponse(JSON.stringify({ message: 'Hello, authenticated world!' }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
