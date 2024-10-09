d21f2341f24a4cf7b96ccdc2d08e31ca962dc537
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-10-09 11:18:52 +0200

    add the PUT method and DELETE on api/user and modify the settings component to modify user data

--- my-app/app/api/user/route.ts
+++ my-app/app/api/user/route.ts
@@ -1,6 +1,14 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 import { getSession } from '@/lib/getSession';
+import formidable from 'formidable';
+
+// Définir un type pour la gestion des fichiers
+export const config = {
+  api: {
+    bodyParser: false, // Désactiver le parsing du corps par défaut
+  },
+};
 
 export async function GET() {
   try {
@@ -27,5 +35,54 @@ export async function GET() {
     return NextResponse.json([user]); // Modifié pour renvoyer un tableau
   } catch (error) {
     return NextResponse.json({ error: 'Error fetching user data' }, { status: 500 });
+  }
+}
+
+// Fonction PUT pour mettre à jour les données utilisateur
+export async function PUT(request: Request) {
+  try {
+    const session = await getSession();
+
+    if (!session || !session.user?.email) {
+      return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+    }
+
+    const { firstName, lastName } = await request.json();
+
+    // Validation des données
+    if (!firstName || !lastName) {
+      return NextResponse.json({ error: 'First name and last name are required' }, { status: 400 });
+    }
+
+    // Mettre à jour les informations de l'utilisateur
+    const updatedUser = await prisma.user.update({
+      where: { email: session.user.email },
+      data: {
+        firstName,
+        lastName,
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    return NextResponse.json({ error: 'Error updating user data' }, { status: 500 });
+  }
+}
+
+// Fonction DELETE pour supprimer un utilisateur
+export async function DELETE() {
+  try {
+    const session = await getSession();
+
+    if (!session || !session.user?.email) {
+      return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+    }
+
+    // Supprimer l'utilisateur
+    await prisma.user.delete({ where: { email: session.user.email } });
+
+    return NextResponse.json({ message: 'User deleted' });
+  } catch (error) {
+    return NextResponse.json({ error: 'Error deleting user' }, { status: 500 });
   }
 }
