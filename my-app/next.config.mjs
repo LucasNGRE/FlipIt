833c6d7532d2a535c262ecaa248a8f8efdc370ee
LucasNGRE <lucas12negre@gmail.com>
2026-04-24 10:07:19 +0200

    allow unsplash and google images in Next.js config

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	env: {
-	  DATABASE_URL: process.env.DATABASE_URL, // Ajoute ici ta variable DATABASE_URL
-	},
-	experimental: {
-	  appDir: true, // Si vous utilisez le dossier 'app' pour les API
-	},
-  };
-  
-  export default nextConfig;
+  reactStrictMode: false,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
+};
+
+export default nextConfig;
   
